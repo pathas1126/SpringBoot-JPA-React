f072c0ce846eac336b062d77a0ce588d0362dcb7
JunHong <pathas@dailybutton.co.kr>
2020-09-24 11:57:17 +0900

    Add Route, Fix Folder Structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Home} from './pages';
 
 function App() {
-	const [message, setMessage] = useState('');
-	const count = useSelector((state) => state.count);
-	const countLoading = useSelector(
-		(state) => state.loading.effects.count.incrementAsync,
-	);
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		axios
-			.get('/api/sayHello')
-			.then((res) => setMessage(res.data))
-			.catch((e) => console.log(e));
-	}, []);
-
-	const increaseCount = () => {
-		dispatch.count.incrementAsync(1);
-	};
-
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				Hello
-				<p>{message}</p>
-				<hr />
-				{count}
-				<button onClick={increaseCount}>+</button>
-				{countLoading && <h2>Loading...</h2>}
-			</header>
-		</div>
+		<>
+			<Route exact path='/' component={Home} />
+		</>
 	);
 }
 
