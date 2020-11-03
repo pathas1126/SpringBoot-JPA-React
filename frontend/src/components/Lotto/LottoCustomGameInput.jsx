6cf4948595b16958a27efdb4674ed0ceab90d940
pathas1126 <pathas@naver.com>
2020-11-03 11:20:06 +0900

    Sperate FrontEnd Project

--- frontend/src/components/Lotto/LottoCustomGameInput.jsx
+++
@@ -1,8 +0,0 @@
-/**@jsx jsx */
-import {jsx, css} from '@emotion/core';
-
-const LottoCustomGameInput = () => {
-	return <input></input>;
-};
-
-export default LottoCustomGameInput;
--- frontend/src/store/models/index.js
+++
@@ -1,2 +0,0 @@
-export {default as lotto} from './lotto';
-export {default as userLotto} from './userLotto';
